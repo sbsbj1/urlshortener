d1bcc8a712e026ea1bed5747b61ed4ebafc23977
sbsbj1 <lucasacevedo095@gmail.com>
2025-07-02 14:28:17 -0400

    Partial save

--- UrlShortener/obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
+++ UrlShortener/obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b120910bd2a07bae9b840fe22b6de67c7197503")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84050f2c897fb37f875acc03b857754e1bdaaaf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
